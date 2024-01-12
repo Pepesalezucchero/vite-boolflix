c180eaf654f1bbb15f4176630d6758ed54f26d7a
Alessio Rugi <alessiorugi95@gmail.com>
2024-01-12 03:41:31 +0100

    store.js aggiunto

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { reactive } from 'vue'
+
+export const store = reactive({
+    filmList: [],
+    apiMoviesUrl: 'https://api.themoviedb.org/3/search/movie?api_key=c4778a5a0028484ac45c14a2d94a23d6',
+    seriesList: [],
+    apiSeriesUrl: 'https://api.themoviedb.org/3/search/tv?api_key=c4778a5a0028484ac45c14a2d94a23d6',
+    posterUrl: 'https://image.tmdb.org/t/p/w342',
+});
